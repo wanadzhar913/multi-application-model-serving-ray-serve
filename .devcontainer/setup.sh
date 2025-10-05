3c383c29b9ba2336f9c4468f64f35a348f551e08
wanadzhar913 <adzhar.faiq@gmail.com>
2025-10-06 00:59:00 +0800

    bump python version

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -8,15 +8,15 @@ apt-get update && apt-get install -y --no-install-recommends \
     wget \
  && add-apt-repository ppa:deadsnakes/ppa \
  && apt-get update && apt-get install -y --no-install-recommends \
-    python3.12 \
-    python3.12-dev \
-    python3.12-venv \
+    python3.13 \
+    python3.13-dev \
+    python3.13-venv \
     poppler-utils \
  && wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py \
- && python3.12 get-pip.py \
+ && python3.13 get-pip.py \
  && rm get-pip.py \
- && ln -sf /usr/bin/python3.12 /usr/bin/python \
- && ln -sf /usr/local/bin/pip3.12 /usr/local/bin/pip \
+ && ln -sf /usr/bin/python3.13 /usr/bin/python \
+ && ln -sf /usr/local/bin/pip3.13 /usr/local/bin/pip \
  && pip install --upgrade pip \
  && pip install uv \
  && apt-get purge -y --auto-remove software-properties-common \
